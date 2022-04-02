550274c8bfd28021c756049ef6d020e62db1902c
mnishimori <mnishimori@yahoo.com.br>
2022-04-02 12:01:41 -0300

    TemplateRef e injeção de TemplateRef

--- src/app/shared/components/modal/services/modal.service.ts
+++ src/app/shared/components/modal/services/modal.service.ts
@@ -1,9 +1,23 @@
-import { Injectable } from '@angular/core';
+import { Injectable, TemplateRef } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ModalService {
 
-  constructor() { }
+  public open(config: ModalConfig): ModalRef {
+    console.log('ModalService open called');
+    return new ModalRef();
+  }
+}
+
+export interface ModalConfig {
+  templateRef: TemplateRef<any>;
+  title: string;
+}
+
+export class ModalRef {
+  public close(): void {
+    console.log('ModalService close called');
+  }
 }
